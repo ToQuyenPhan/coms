74a9c75968519c297dec8c84217eb1198b0eb833
ToQuyenPhan <duonglh024@gmail.com>
2023-10-30 17:43:39 +0700

    update

--- Coms.Api/Program.cs
+++ Coms.Api/Program.cs
@@ -16,12 +16,12 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+//if (app.Environment.IsDevelopment())
+//{
+    
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
