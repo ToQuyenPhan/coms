8cc6822cd8ca3d1475d6eb0548221039d83464fb
ToQuyenPhan <duonglh024@gmail.com>
2023-10-30 18:01:17 +0700

    update url

--- Coms.Api/Program.cs
+++ Coms.Api/Program.cs
@@ -18,10 +18,14 @@ var app = builder.Build();
 //Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    
+    app.UseDeveloperExceptionPage();
 }
 app.UseSwagger();
-app.UseSwaggerUI(); 
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Coms API V1");
+    c.RoutePrefix = "";
+});
 
 app.UseHttpsRedirection();
 
