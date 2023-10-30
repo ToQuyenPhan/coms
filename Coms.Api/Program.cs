24d854d757d98b6dcd15fd955cc10650d166d0ea
ToQuyenPhan <duonglh024@gmail.com>
2023-10-31 00:32:36 +0700

    add jwt bearer

--- Coms.Api/Program.cs
+++ Coms.Api/Program.cs
@@ -2,6 +2,7 @@ using Coms.Api.Common.Errors;
 using Coms.Application;
 using Coms.Infrastructure;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,35 @@ builder.Services.AddControllers();
 builder.Services.AddSingleton<ProblemDetailsFactory, ComsProblemDetailsFactory>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Coms.API", Version = "v1" });
+    c.EnableAnnotations();
+    var securityScheme = new OpenApiSecurityScheme()
+    {
+        Description = "JWT Authorization header using the Bearer scheme. " +
+                                    "\n\nEnter 'Bearer' [space] and then your token in the text input below. " +
+                                      "\n\nExample: 'Bearer 12345abcde'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        Reference = new OpenApiReference()
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer"
+        }
+    };
+    c.AddSecurityDefinition("Bearer", securityScheme);
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        securityScheme,
+                        new string[]{ }
+                    }
+                });
+});
 
 var app = builder.Build();
 
@@ -28,7 +57,7 @@ app.UseSwaggerUI(c =>
 });
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
