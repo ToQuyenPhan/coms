f3a218ac66e41cdd942706e771f9a7c2fbb5c750
ToQuyenPhan <duonglh024@gmail.com>
2023-12-08 17:10:27 +0700

    update database

--- Coms.Domain/Entities/ContractField.cs
+++ Coms.Domain/Entities/ContractField.cs
@@ -1,17 +1,22 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Coms.Domain.Entities
 {
     public class ContractField
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
         [Required]
-        public string Content { get; set; }
+        public string FieldName { get; set; } = string.Empty;
+
+        [Required]
+        public string Content { get; set; } = string.Empty;
 
         public int? ContractId { get; set; }
-        public virtual Contract Contract { get; set; }
-
-        public int? TemplateFieldId { get; set; }
-        public virtual TemplateField TemplateField { get; set; }
+        public virtual Contract? Contract { get; set; }
 
     }
 }
