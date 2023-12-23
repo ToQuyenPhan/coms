5e1a8d389a97bef98ace17e21838154696282a35
ToQuyenPhan <duonglh024@gmail.com>
2023-12-23 13:28:40 +0700

    add Get Contracts For Managers

--- Coms.Application/Services/Contracts/ContractService.cs
+++ Coms.Application/Services/Contracts/ContractService.cs
@@ -644,97 +644,107 @@ namespace Coms.Application.Services.Contracts
         public async Task<ErrorOr<PagingResult<ContractResult>>> GetManagerContracts(int userId,
                 string name, string creatorName, int? status, int currentPage, int pageSize)
         {
-            //if (_userAccessRepository.GetYourAccesses(userId).Result is not null)
-            //{
-            //    if (string.IsNullOrEmpty(creatorName))
-            //    {
-            //        creatorName = "";
-            //    }
-            //    var predicate = PredicateBuilder.New<Access>(true);
-            //predicate = predicate.And(a => a.Contract.Status != DocumentStatus.Deleted);
-            //if (!string.IsNullOrEmpty(name))
-            //{
-            //    predicate = predicate.And(a => a.Contract.ContractName.Contains(name.Trim()));
-            //}
-            //if (status is not null)
-            //{
-            //    if (status >= 0)
-            //    {
-            //        predicate = predicate.And(c => c.Contract.Status.Equals((DocumentStatus)status));
-            //    }
-            //}
-            //var yourAccesses = _userAccessRepository.GetYourAccesses(userId).Result;
-            //IList<Access> accesses = new List<Access>();
-            //foreach (var userAccess in yourAccesses)
-            //{
-            //    if (userAccess.User.Email.Contains(creatorName))
-            //    {
-            //        var access = await _accessRepository.GetManagerAccess((int)userAccess.AccessId);
-            //        if (access is not null)
-            //        {
-            //            var approveWorkflow = await _aproveWorkflowRepository.GetByAccessId(access.Id);
-            //            if(approveWorkflow is not null)
-            //            {
-            //                if (approveWorkflow.Status.Equals(ApproveWorkflowStatus.Waiting))
-            //                {
-            //                    if (!accesses.Contains(access))
-            //                    {
-            //                        accesses.Add(access);
-            //                    }
-            //                }
-            //            }
-            //        }
-            //    }
-            //}
-            //IList<Access> filteredList = accesses.Where(predicate).ToList();
-            //var total = filteredList.Count();
-            //if (currentPage > 0 && pageSize > 0)
-            //{
-            //    filteredList = filteredList.Skip((currentPage - 1) * pageSize).Take(pageSize)
-            //            .ToList();
-            //}
-            //IList<ContractResult> responses = new List<ContractResult>();
-            //foreach (var contract in filteredList)
-            //{
-            //    var contractResult = new ContractResult()
-            //    {
-            //        Id = contract.Contract.Id,
-            //        ContractName = contract.Contract.ContractName,
-            //        Version = contract.Contract.Version,
-            //        CreatedDate = contract.Contract.CreatedDate,
-            //        CreatedDateString = contract.Contract.CreatedDate.Date.ToString("dd/MM/yyyy"),
-            //        UpdatedDate = contract.Contract.UpdatedDate,
-            //        UpdatedDateString = contract.Contract.UpdatedDate.ToString(),
-            //        EffectiveDate = contract.Contract.EffectiveDate,
-            //        EffectiveDateString = contract.Contract.EffectiveDate.ToString(),
-            //        Status = (int)contract.Contract.Status,
-            //        StatusString = contract.Contract.Status.ToString(),
-            //        TemplateID = contract.Contract.TemplateId,
-            //        Code = contract.Contract.Code,
-            //        Link = contract.Contract.Link
-            //    };
-            //    var access = await _userAccessRepository.GetByAccessId(contract.Id);
-            //    if (contract.AccessRole.Equals(AccessRole.Author))
-            //    {
-            //        contractResult.CreatorId = access.User.Id;
-            //        contractResult.CreatorName = access.User.FullName;
-            //        contractResult.CreatorEmail = access.User.Email;
-            //        contractResult.CreatorImage = access.User.Image;
-            //    }
-            //    var partner = await _partnerReviewRepository.GetByContractId(contract.Contract.Id);
-            //    contractResult.PartnerId = partner.Partner.Id;
-            //    contractResult.PartnerName = partner.Partner.CompanyName;
-            //    responses.Add(contractResult);
-            //}
-            //return new
-            //    PagingResult<ContractResult>(responses, total, currentPage, pageSize);
-            return Error.NotFound();
-            //}
-            //else
-            //{
-            //    return new PagingResult<ContractResult>(new List<ContractResult>(), 0, currentPage,
-            //        pageSize);
-            //}
+            IList<Contract> contracts = new List<Contract>();
+            var flowDetails = await _flowDetailRepository.GetUserFlowDetailsByUserId(userId);
+            if (flowDetails is not null)
+            {
+                foreach (var flowDetail in flowDetails)
+                {
+                    var contractFlowDetails = await _contractFlowDetailsRepository.GetByFlowDetailId(flowDetail.Id);
+                    if (contractFlowDetails is not null)
+                    {
+                        foreach (var contractFlowDetail in contractFlowDetails)
+                        {
+                            var contract = await _contractRepository.GetContract(contractFlowDetail.ContractId);
+                            if (!contract.Status.Equals(DocumentStatus.Deleted))
+                            {
+                                var existedContract = contracts.FirstOrDefault(c => c.Id.Equals(contract.Id));
+                                if (existedContract is null)
+                                {
+                                    if (!string.IsNullOrEmpty(contract.Link))
+                                    {
+                                        contracts.Add(contract);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (contracts.Count() > 0)
+            {
+                //if (string.IsNullOrEmpty(creatorName))
+                //{
+                //    creatorName = "";
+                //}
+                var predicate = PredicateBuilder.New<Contract>(true);
+                predicate = predicate.And(c => c.Status != DocumentStatus.Deleted);
+                if (!string.IsNullOrEmpty(name))
+                {
+                    predicate = predicate.And(c => c.ContractName.Contains(name.Trim()));
+                }
+                if (status is not null)
+                {
+                    if (status >= 0)
+                    {
+                        predicate = predicate.And(c => c.Status.Equals((DocumentStatus)status));
+                    }
+                }
+                IList<Contract> filteredList = contracts.Where(predicate).OrderByDescending(c => c.CreatedDate)
+                        .ToList();
+                var total = filteredList.Count();
+                if (currentPage > 0 && pageSize > 0)
+                {
+                    filteredList = filteredList.Skip((currentPage - 1) * pageSize).Take(pageSize)
+                            .ToList();
+                }
+                IList<ContractResult> responses = new List<ContractResult>();
+                foreach (var contract in filteredList)
+                {
+                    var contractResult = new ContractResult()
+                    {
+                        Id = contract.Id,
+                        ContractName = contract.ContractName,
+                        Version = contract.Version,
+                        CreatedDate = contract.CreatedDate,
+                        CreatedDateString = contract.CreatedDate.Date.ToString("dd/MM/yyyy"),
+                        EffectiveDate = contract.EffectiveDate,
+                        EffectiveDateString = contract.EffectiveDate.ToString(),
+                        Status = (int)contract.Status,
+                        StatusString = contract.Status.ToString(),
+                        TemplateID = contract.TemplateId,
+                        Code = contract.Code,
+                        Link = contract.Link
+                    };
+                    if (contract.UpdatedDate is not null)
+                    {
+                        contractResult.UpdatedDate = contract.UpdatedDate;
+                        contractResult.UpdatedDateString = contract.UpdatedDate.ToString();
+                    }
+                    var actionHistory = await _actionHistoryRepository.GetCreateActionByContractId(contract.Id);
+                    if (actionHistory is not null)
+                    {
+                        contractResult.CreatorId = actionHistory.User.Id;
+                        contractResult.CreatorName = actionHistory.User.FullName;
+                        contractResult.CreatorEmail = actionHistory.User.Email;
+                        contractResult.CreatorImage = actionHistory.User.Image;
+                    }
+                    var partner = await _partnerReviewRepository.GetByContractId(contract.Id);
+                    if (partner is not null)
+                    {
+                        contractResult.PartnerId = partner.Partner.Id;
+                        contractResult.PartnerName = partner.Partner.CompanyName;
+                    }
+                    responses.Add(contractResult);
+                }
+                return new
+                    PagingResult<ContractResult>(responses, total, currentPage, pageSize);
+            }
+            else
+            {
+                return new PagingResult<ContractResult>(new List<ContractResult>(), 0, currentPage,
+                    pageSize);
+            }
         }
 
         public async Task<ErrorOr<PagingResult<ContractResult>>> GetContractForPartner(int partnerId,
