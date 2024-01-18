9f61d251875e669c15b3f2bbdc94b6535ccf85e4
ToQuyenPhan <duonglh024@gmail.com>
2024-01-18 13:36:37 +0700

    update get template fields

--- Coms.Application/Services/TemplateFields/TemplateFieldService.cs
+++ Coms.Application/Services/TemplateFields/TemplateFieldService.cs
@@ -215,7 +215,7 @@ namespace Coms.Application.Services.TemplateFields
                             Content = content,
                         };
                         templateFieldResult.Type = "text";
-                        if (templateFieldResult.Name.Contains("Duration"))
+                        if (templateFieldResult.Name.Contains("Duration") || templateFieldResult.Name.Equals("Execution Time"))
                         {
                             templateFieldResult.Type = "number";
                             templateFieldResult.MinValue = 0;
